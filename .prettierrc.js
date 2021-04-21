a612a8a82a4cb9b36123dd4bfbaea1ea5ac8a8c5
Sandrico Provo <Sprovo@outlook.com>
2021-04-21 10:42:01 -0300

    Add .prettierrc.js file for vscode

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: 'es5',
+    singleQuote: true,
+    printWidth: 100,
+    bracketSpacing: true,
+    tabWidth: 4,
+    // Helps with Windows users facing CLRF eslint/prettier error
+    endOfLine: 'auto',
+    jsxBracketSameLine: false,
+};
